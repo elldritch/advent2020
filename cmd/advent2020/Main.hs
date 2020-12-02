273a5428dae9a5f0f929be94e31d7d13737a5315
Leo Zhang <leo@leozhang.me>
2020-12-02 14:19:25 -0800

    Day 2 part 2

--- cmd/advent2020/Main.hs
+++ cmd/advent2020/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import qualified Advent2020.D1 as D1 (run, part1, part2)
-import qualified Advent2020.D2 as D2 (run)
+import qualified Advent2020.D2 as D2 (run, part1, part2)
 import Options.Applicative (ParserInfo, auto, briefDesc, execParser, helper, info, long, option, progDesc, strOption)
 import Relude
 
@@ -35,7 +35,7 @@ main = do
       2 -> print $ D1.run contents D1.part2
       _ -> putTextLn "No such part"
     2 -> case part of
-      1 -> print $ D2.run contents
-      2 -> undefined
+      1 -> print $ D2.run contents D2.part1
+      2 -> print $ D2.run contents D2.part2
       _ -> putTextLn "No such part"
     _ -> putTextLn "No such puzzle"
--- src/Advent2020/D2.hs
+++ src/Advent2020/D2.hs
@@ -1,15 +1,15 @@
-module Advent2020.D2 (parse, Password(..), errorBundlePretty, isValid, run) where
+module Advent2020.D2 (run, part1, part2, parse, Password (..), errorBundlePretty) where
 
 import Relude hiding (max, min, some)
-import Relude.Unsafe (read)
-import Text.Megaparsec ((<?>), errorBundlePretty, ParseErrorBundle, Parsec, chunk, manyTill, runParser, some)
-import Text.Megaparsec.Char (spaceChar, char, letterChar, newline, numberChar)
+import Relude.Unsafe (read, (!!))
+import Text.Megaparsec (ParseErrorBundle, Parsec, chunk, errorBundlePretty, manyTill, runParser, some, (<?>))
+import Text.Megaparsec.Char (char, letterChar, newline, numberChar, spaceChar)
 
 data Password = Password
-  { min :: Int,
-    max :: Int,
+  { a :: Int,
+    b :: Int,
     letter :: Char,
-    password :: Text
+    password :: String
   }
   deriving (Show, Eq)
 
@@ -20,27 +20,32 @@ type Parser = Parsec Void Text
 
 parser :: Parser Password
 parser = do
-  min' <- numberChar `manyTill` char '-' <?> "min"
-  let min :: Int = read min'
-  max' <- numberChar `manyTill` spaceChar <?> "max"
-  let max :: Int = read max'
+  a' <- numberChar `manyTill` char '-' <?> "a"
+  let a :: Int = read a'
+  b' <- numberChar `manyTill` spaceChar <?> "b"
+  let b :: Int = read b'
   letter <- letterChar <?> "letter"
   _ <- chunk ": "
-  password <- toText <$> letterChar `manyTill` newline <?> "password"
+  password <- letterChar `manyTill` newline <?> "password"
   return Password {..}
 
-isValid :: Password -> Bool
-isValid Password{..} = count >= min && count <= max
+run :: Text -> (Password -> Bool) -> Int
+run contents isValid = sum $ map fromEnum valids
+  where
+    passwords = case parse contents of
+      Right ps -> ps
+      Left err -> error $ toText $ errorBundlePretty err
+    valids = map isValid passwords
+
+part1 :: Password -> Bool
+part1 Password {..} = count >= a && count <= b
   where
     countLetter :: Char -> String -> Int
     countLetter l s = foldr (\c n -> if c == l then n + 1 else n) 0 s
 
     count = countLetter letter $ toString password
 
-run :: Text -> Int
-run contents = sum $ map fromEnum valids
+part2 :: Password -> Bool
+part2 Password {..} = letterAt a `xor` letterAt b
   where
-    passwords = case parse contents of
-      Right ps -> ps
-      Left err -> error $ toText $ errorBundlePretty err
-    valids = map isValid passwords
+    letterAt i = password !! (i - 1) == letter
