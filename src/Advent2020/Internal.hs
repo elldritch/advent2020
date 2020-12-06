87c712927a3c31873b975c6f1f5a51db000e9488
Leo Zhang <leo@leozhang.me>
2020-12-06 11:53:46 -0800

    doc: Haddock for common utilities

--- src/Advent2020/Internal.hs
+++ src/Advent2020/Internal.hs
@@ -16,33 +16,34 @@ import Data.Either.Combinators (mapLeft)
 import Relude
 import Text.Megaparsec (Parsec, errorBundlePretty, runParser)
 
--- | readEither specialized to integers, with clearer error message.
+-- | 'readEither' specialized to integers, with clearer error message.
 readInt :: (ToString s) => s -> Either Text Int
 readInt s = mapLeft (const $ toText $ "could not parse as integer: " ++ show s') $ readEither s'
   where
     s' = toString s
 
--- | map with index.
+-- | 'map' over a list while providing an index with each element.
 map' :: (Int -> a -> b) -> [a] -> [b]
 map' f xs = uncurry f <$> zip [0 ..] xs
 
--- | map to Eithers with error messages indicating indexes of elements that
--- | mapped to a Left.
+-- | 'map' to 'Either's with error messages indicating the indexes of elements
+-- that mapped to a 'Left'.
 mapE :: (a -> Either Text b) -> [a] -> [Either Text b]
 mapE f = map' f'
   where
     f' i x = mapLeft (\e -> "error while processing element at index " <> show i <> ": " <> e) $ f x
 
--- | mapE, handling error message concatenation.
+-- | 'mapE', with pretty error message concatenation.
 mapE' :: (a -> Either Text b) -> [a] -> Either Text [b]
 mapE' f xs = gather' $ mapE f xs
 
--- | Like Megaparsec's label, but for nice error messages with Either Text.
+-- | Like Megaparsec's 'Text.Megaparsec.label', but for nice error messages with
+-- @'Either' 'Text'@.
 label :: Text -> Either Text t -> Either Text t
 label l = mapLeft (\err -> "error while " <> l <> ": " <> err)
 
 -- | Takes a list of successes or failures. On any failure, produces a list of
--- | failures. Otherwise, produces a list of successes.
+-- failures. Otherwise, produces a list of successes.
 gather :: [Either e a] -> Either [e] [a]
 gather = foldr foldEither (Right [])
   where
@@ -55,21 +56,24 @@ gather = foldr foldEither (Right [])
         Right _ -> Left ls
         Left l -> Left (l : ls)
 
--- | gather specialized to Either Text, with error message concatenation.
+-- | 'gather' specialized to @'Either' 'Text'@, with pretty error message
+-- concatenation.
 gather' :: [Either Text a] -> Either Text [a]
 gather' = mapLeft gatherErrors . gather
   where
     gatherErrors :: [Text] -> Text
     gatherErrors es = mconcat $ intersperse ", " es
 
--- | Parsers on Text without custom error components.
+-- | Parsers on 'Text' without custom error components. See the documentation
+-- for "Text.Megaparsec".
 type Parser = Parsec Void Text
 
--- | Runs a parser, pretty-printing error messages to Text.
+-- | Runs a parser, pretty-printing error messages to 'Text'.
 parseWithPrettyErrors :: Parser t -> Text -> Either Text t
 parseWithPrettyErrors parser contents = mapLeft (toText . errorBundlePretty) $ runParser parser "" contents
 
--- | Transform and validate a parsed value.
+-- | Transform and validate a parsed value. If the validation fails, the parser
+-- fails as well.
 parseWith :: (s -> Either Text t) -> Parser s -> Parser t
 parseWith f p = do
   x <- p
