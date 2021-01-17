98893c3a37a32f5201c686cddaa4dcf6da3632d9
Leo Zhang <leo@fossa.io>
2021-01-17 00:56:47 -0800

    refactor: Use more idiomatic State rather than terminating on Except

--- src/Advent2020/Internal/D22.hs
+++ src/Advent2020/Internal/D22.hs
@@ -9,7 +9,7 @@ module Advent2020.Internal.D22
   )
 where
 
-import Advent2020.Internal (integralP, parseWithPrettyErrors, symbol, unsafeNonEmpty)
+import Advent2020.Internal (tracePrefix, integralP, parseWithPrettyErrors, symbol, unsafeNonEmpty)
 import Control.Monad.Combinators.NonEmpty (some)
 import Data.Set (insert, member)
 import Data.Tuple.Extra (both)
@@ -58,34 +58,29 @@ play g = case step g of
 score :: Deck -> Int
 score d = foldr (\(n, card) acc -> n * card + acc) 0 $ zip [1 ..] $ reverse $ toList d
 
-step' :: Set Game -> Game -> Either (Player, Deck) (Set Game, Game)
-step' seen g@(d1, d2) =
-  if member g seen
-    then Left (One, d1)
-    else do
-      d1Next <- maybeToRight (Two, unsafeNonEmpty d2Played) $ nonEmpty d1Played
-      d2Next <- maybeToRight (One, unsafeNonEmpty d1Played) $ nonEmpty d2Played
-      return (insert g seen, (d1Next, d2Next))
-  where
-    c1 = head d1
-    c2 = head d2
-    d1Drawn = tail d1
-    d2Drawn = tail d2
-    winner
-      -- Case: play sub-game, victor wins.
-      | c1 <= length d1Drawn && c2 <= length d2Drawn =
-        fst $ play' (both unsafeNonEmpty (take c1 d1Drawn, take c2 d2Drawn))
-      -- Case: no sub-game possible, higher value wins.
-      | c1 > c2 = One
-      | otherwise = Two
-    (d1Played, d2Played) = case winner of
-      One -> (d1Drawn <> [c1, c2], d2Drawn)
-      Two -> (d1Drawn, d2Drawn <> [c2, c1])
-
 play' :: Game -> (Player, Deck)
-play' = f mempty
+play' = runIdentity . evalStateT f . (mempty,)
   where
-    f :: Set Game -> Game -> (Player, Deck)
-    f seen g = case step' seen g of
-      Right (seen', g') -> f seen' g'
-      Left l -> l
+    f :: State (Set Game, Game) (Player, Deck)
+    f = do
+      (seen, g@(d1, d2)) <- get
+      let c1 = head d1
+      let c2 = head d2
+      let d1Drawn = tail d1
+      let d2Drawn = tail d2
+      let winner
+            -- Case: play sub-game, victor wins.
+            | c1 <= length d1Drawn && c2 <= length d2Drawn =
+              fst $ play' (both unsafeNonEmpty (take c1 d1Drawn, take c2 d2Drawn))
+            -- Case: no sub-game possible, higher value wins.
+            | c1 > c2 = One
+            | otherwise = Two
+      let (d1Played, d2Played) = case winner of
+            One -> (d1Drawn <> [c1, c2], d2Drawn)
+            Two -> (d1Drawn, d2Drawn <> [c2, c1])
+      let action
+            | member g seen = return (One, d1)
+            | null d1Played = return (Two, unsafeNonEmpty d2Played)
+            | null d2Played = return (One, unsafeNonEmpty d1Played)
+            | otherwise = put (insert g seen, (unsafeNonEmpty d1Played, unsafeNonEmpty d2Played)) >> f
+      action
